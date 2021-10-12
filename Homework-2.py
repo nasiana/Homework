49532eb00f2d6950636f2d3feecdeecb3d995990
nasiana <87439662+nasiana@users.noreply.github.com>
2021-10-12 16:44:49 +0100

    Add files via upload

---
+++ Homework-2.py
@@ -0,0 +1,22 @@
+# incorrect version TASK 3 (PYTHON Q1)
+chairs = '15'
+nails = 4
+total_nails = chairs * nails
+message = 'I need to buy {} nails'.format(total_nails)
+print('You need to buy {} nails'.format(message))
+
+# corrected version TASK 3 (PYTHON Q1)
+chairs = 15
+nails = 4
+total_nails = chairs * nails
+message = 'I need to buy {} nails'.format(total_nails)
+print('{}'.format(message))
+
+# incorrect version TASK 3 (PYTHON Q2)
+my_name = Penelope
+
+my_age = 29
+
+message = 'My name is {} and I am {} years old'.format(my_name, my_age)
+
+print(message)
