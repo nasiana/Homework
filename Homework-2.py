bb621210f6a1ae98d1db3faa91de9b447705af64
nasiana <87439662+nasiana@users.noreply.github.com>
2021-10-13 01:04:01 +0100

    Add files via upload

--- Homework-2.py
+++ Homework-2.py
@@ -33,18 +33,27 @@ eggs = x = 1
 
 # BOX: b can be varied
 b = 6
+o = 4
 box = b * x
-total_boxes = box/b
-print("{} is the total number of eggs in {} boxes".format(box, total_boxes))
+number_boxes = box/b
+print("{} is the total number of eggs in {} boxes".format(box, number_boxes))
+total_b_varied = 6
+total_b_o = ((total_b_varied) * box)/o
+print("With {} boxes of eggs, you can make {} omelettes.".format(total_b_varied,total_b_o))
 
 # Omelette: o can be varied
 o = 4
+b = 6
 omelette = o * x
-total_omelette = omelette/o
-print("{} is the total number of eggs in {} omelette".format(omelette, total_omelette))
+number_omelette = omelette/o
+print("{} is the total number of eggs in {} omelette".format(omelette, number_omelette))
+total_o_varied = 9
+total_o_b = ((total_o_varied) * omelette)/b
+print("You can make {} omelettes with {} boxes of eggs.".format(total_o_varied, total_o_b))
 
-# final
+# general statement/sanity check
 omelette_box = box/o
-print("You can make {} omelettes with {} boxes of eggs".format(omelette_box,total_boxes))
+print("You can make {} omelettes with {} boxes of eggs".format(omelette_box,number_boxes))
+
 
 
