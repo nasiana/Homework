5d434f433e5f2c60615372a51d0509ee488a1e28
nasiana <87439662+nasiana@users.noreply.github.com>
2021-10-13 15:37:54 +0100

    Add files via upload

--- Homework-2.py
+++ Homework-2.py
@@ -38,7 +38,7 @@ box = b * x
 number_boxes = box/b
 print("{} is the total number of eggs in {} boxes".format(box, number_boxes))
 # total_b_varied is the variable to be varied by user
-total_b_varied = 6
+total_b_varied = 10
 total_b_o = ((total_b_varied) * box)/o
 print("With {} boxes of eggs, you can make {} omelettes.".format(total_b_varied,total_b_o))
 
@@ -49,7 +49,7 @@ omelette = o * x
 number_omelette = omelette/o
 print("{} is the total number of eggs in {} omelette".format(omelette, number_omelette))
 # total_o_varied is the variable to be varied by user
-total_o_varied = 9
+total_o_varied = 15
 total_o_b = ((total_o_varied) * omelette)/b
 print("You can make {} omelettes with {} boxes of eggs.".format(total_o_varied, total_o_b))
 
@@ -57,5 +57,18 @@ print("You can make {} omelettes with {} boxes of eggs.".format(total_o_varied, 
 omelette_box = box/o
 print("You can make {} omelettes with {} boxes of eggs".format(omelette_box,number_boxes))
 
+# mathematical relationships
+o = 4
+b = 6
+relation_o = o/b
+# o/b = 4/6
+print("{} boxes of eggs is required to make {} omelettes.".format(relation_o, 1))
+relation_b = b/o
+# b/o = 6/4
+print("{} omelettes can be made from {} boxes of eggs".format(relation_b, 1))
 
-
+# method 2 of doing the question, user must vary total_o_varied and total_b_varied
+user_boxes = relation_o * total_o_varied
+print("With {} boxes of eggs, you can make {} omelettes.".format(user_boxes,total_o_varied))
+user_omellettes = relation_b * total_b_varied
+print("You can make {} omelettes with {} boxes of eggs.".format(user_omellettes, total_b_varied))
