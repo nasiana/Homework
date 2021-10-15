cbb9fb34af509fb2b133fc9a835f7ebaffa48ab4
Nasian <87439662+nasiana@users.noreply.github.com>
2021-10-15 19:01:33 +0100

    Add files via upload

--- HW2.sql
+++ HW2.sql
@@ -1,3 +1,7 @@
 USE BAKERY;
 select * from sweet;
 create index item_name_idx on sweet(item_name);
+
+use bank;
+select * FROM accounts;
+CREATE INDEX mult_col_idx_accounts ON accounts(account_number, account_holder_name, account_holder_surname);
