4d655910cd7a491dd19ca210ddbdc83b873123f3
Nasian <87439662+nasiana@users.noreply.github.com>
2021-10-22 00:29:06 +0100

    Add files via upload

--- HW3.py
+++ HW3.py
@@ -31,4 +31,38 @@ if my_money >= boat_cost:
 else:
 	print('You cannot afford the board hire')
 
+# Q3
+# constraint: that sells books between 1800 and 1950
+user_year = int(input("What was the year the book was sold in? "))
+if user_year < 1800 or user_year > 1950:
+    print("Books in this shop are sold between 1800 and 1950, please check the year and enter again")
+
+from math import floor
+
+def year_century():
+    # // is the floor method
+    century = (user_year//100) + 1
+    # as we are only dealing with the time period 1800 - 1950 the names are set otherwise this would get more complex
+    if century == 19:
+        num_2_string = 'Nineteenth'
+        return ("{} Century").format(num_2_string)
+    if century == 20:
+        num_2_string = 'Twentieth'
+        return ("{} Century").format(num_2_string)
+
+
+
+def number_decade():
+    decade_1 = int(str(user_year)[2:])
+    decade_2 = decade_1 // 10
+    number2decade = {'1': "Oneties", '2': "Twenties", '3': "Thirties", '4': "Forties", '5': "Fifties", '6': "Sixties",
+            '7': "Seventies", '8': "Eighties", '9': "Nineties", '0': "Noughties"}
+    return " ".join(map(lambda i: number2decade[i], str(decade_2)))
+
+year_century = year_century()
+number_decade = number_decade()
+print(("The year book was from {}. The century and decade: {}, {}.").format(user_year, year_century, number_decade))
+
+
+
 
