2f48f8664afac134ed033a2462276fd9241dc010
Nasian <87439662+nasiana@users.noreply.github.com>
2021-10-22 15:11:35 +0100

    Add files via upload

--- hw3.py
+++ hw3.py
@@ -143,3 +143,10 @@ print(lottery_prizes)
 TASK 3  (Read and Write files)
 '''
 
+# Q2
+
+# Example solution
+
+poem = 'I like Python and I am not very good at poems'
+with open('poem.txt', 'w') as poem_file:
+poem_file.write(poem)
