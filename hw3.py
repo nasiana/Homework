a4da3a4b106ee622408817af66e1e92e7dce1161
Nasian <87439662+nasiana@users.noreply.github.com>
2021-10-22 15:42:18 +0100

    Add files via upload

--- hw3.py
+++ hw3.py
@@ -150,3 +150,27 @@ TASK 3  (Read and Write files)
 poem = 'I like Python and I am not very good at poems'
 with open('poem.txt', 'w') as poem_file:
 poem_file.write(poem)
+
+# Q3
+
+# TASK 1
+
+elton_lyrics =
+"
+"You could never know what it's like\
+\nYour blood like winter freezes just like ice\
+\nAnd there's a cold lonely light that shines from you\
+\nYou'll wind up like the wreck you hide behind that mask you\
+use\
+\nAnd did you think this fool could never win?\
+\nWell look at me, I'm coming back again\
+\nI got a taste of love in a simple way\
+\nAnd  if  you  need  to   know   while  I'm   still   standing,  you  just\
+fade away\
+\nDon't you know I'm still standing better than I ever did\
+\nLooking like a true survivor, feeling like a little kid\
+\nI'm still standing after all this time\
+\nPicking up the pieces of my life without you on my mind\
+\nI'm still standing (Yeah, yeah, yeah)\
+\nI'm still standing (Yeah, yeah, yeah)\
+"
